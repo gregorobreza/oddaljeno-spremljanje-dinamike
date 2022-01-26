d5f89c85bca0d66031e1cafdaf61e7a84799c228
gregorobreza <gobreza@gmail.com>
2022-01-26 11:15:45 +0100

    prikaz vseh meritev

--- mqtt_proxy/measurement/models.py
+++ mqtt_proxy/measurement/models.py
@@ -6,6 +6,7 @@ class Measurement(models.Model):
     title = models.CharField(max_length=250)
     json_file = models.FileField(upload_to="measurements/")
     npz_file = models.FileField(upload_to="measurements/")
+    comment = models.CharField(max_length=50)
     added = models.DateTimeField(auto_now_add=True)
 
 
