0ed9921d4166f03be16cad1e99659f4fb4ac6c6c
gregorobreza <gobreza@gmail.com>
2022-03-06 08:30:29 +0100

    Update settings.py

--- mqtt_proxy/mqtt_proxy/settings.py
+++ mqtt_proxy/mqtt_proxy/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k*ip53(8e@md$^)o!8+9bh!(qny8vl)c6*6a-hre422-6d(h2m'
+SECRET_KEY = 'something_secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
